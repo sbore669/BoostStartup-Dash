64ea85f53cefbe6a7130e03bf348d3c665b71919
sbore669 <107039494+sbore669@users.noreply.github.com>
2023-03-08 13:56:49 +0000

    push sout terminier

--- src/app/layouts/sidebar/sidebar.component.ts
+++ src/app/layouts/sidebar/sidebar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/_services/auth.service';
+import { StorageService } from 'src/app/_services/storage.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService,
+    private storageService: StorageService,
+    private router: Router,) { }
 
   ngOnInit(): void {
+  }
+  logout(): void {
+    this.authService.logout().subscribe({
+      next: res => {
+        console.log(res);
+        this.storageService.clean();
+
+        // window.location.reload();
+        this.router.navigate(['/login']);
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
   }
 
 }
